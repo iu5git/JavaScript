953ec62b2c804c32478736708f05ada37a8a639f
Alekhin Sergey <seralekhin@ozon.ru>
2023-01-30 01:29:08 +0300

    init lab3

---
+++ tutorials/front/example/components/back-button/index.js
@@ -0,0 +1,25 @@
+export class BackButtonComponent {
+    constructor(parent) {
+        this.parent = parent;
+    }
+
+    addListeners(listener) {
+        document
+            .getElementById("back-button")
+            .addEventListener("click", listener)
+    }
+
+    getHTML() {
+        return (
+            `
+                <button id="back-button" class="btn btn-primary" type="button">Назад</button>
+            `
+        )
+    }
+
+    render(listener) {
+        const html = this.getHTML()
+        this.parent.insertAdjacentHTML('beforeend', html)
+        this.addListeners(listener)
+    }
+}
---
+++ tutorials/front/example/main.js
@@ -0,0 +1,6 @@
+import {MainPage} from "./pages/main/index.js";
+
+const root = document.getElementById('root');
+
+const mainPage = new MainPage(root);
+mainPage.render();
